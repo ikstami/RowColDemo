1df63a81ece0d1656c21ad9d2e09440d6ac99576
ikstami <k.mus00@yandex.ru>
2025-11-13 19:50:30 +0700

    доп смещение

--- app/src/main/java/com/hfad/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/hfad/rowcoldemo/MainActivity.kt
@@ -55,9 +55,19 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
-        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
-        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
-        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
+        Text(
+            text = "Large Text\n\nMore Text",
+            Modifier.alignBy(FirstBaseline),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Small Text",
+            modifier = Modifier.paddingFrom(
+                alignmentLine = FirstBaseline, before = 80.dp, after = 0.dp),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 
 
